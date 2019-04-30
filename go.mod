94a92721589b1c7f6040c3520f4243d50c0e83c7
Vitaly Isaev <vitaly.isaev@ncloudtech.ru>
2019-04-30 14:09:32 +0300

    add sources

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/vitalyisaev2/pprof_cgo_issue
+
+go 1.12
+
+require (
+	github.com/ianlancetaylor/cgosymbolizer v0.0.0-20170921033129-f5072df9c550
+	github.com/pkg/profile v1.3.0
+	github.com/spacemonkeygo/openssl v0.0.0-20181017203307-c2dcc5cca94a
+)
---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"fmt"
+	"hash"
+	"log"
+	"runtime"
+	"sync"
+
+	_ "github.com/ianlancetaylor/cgosymbolizer"
+
+	"github.com/pkg/profile"
+	"github.com/spacemonkeygo/openssl"
+)
+
+var _ hash.Hash = (*openSSLSHA256Hash)(nil)
+
+type openSSLSHA256Hash struct {
+	*openssl.SHA256Hash
+}
+
+func (h *openSSLSHA256Hash) BlockSize() int { return sha256.BlockSize }
+
+func (h *openSSLSHA256Hash) Reset() {
+	if err := h.SHA256Hash.Reset(); err != nil {
+		panic(err)
+	}
+}
+
+func (h *openSSLSHA256Hash) Size() int { return sha256.Size }
+
+func (h *openSSLSHA256Hash) Sum(p []byte) []byte {
+	s, err := h.SHA256Hash.Sum()
+	if err != nil {
+		panic(err)
+	}
+	return append(p, s[:]...)
+}
+
+func newOpenSSLSHA256Hash() hash.Hash {
+	h, err := openssl.NewSHA256Hash()
+	if err != nil {
+		panic(err)
+	}
+	return &openSSLSHA256Hash{
+		SHA256Hash: h,
+	}
+}
+
+func compute(pool *sync.Pool, data []byte) ([]byte, error) {
+	h := pool.Get().(hash.Hash)
+	defer pool.Put(h)
+
+	h.Reset()
+	_, err := h.Write(data)
+	if err != nil {
+		return nil, err
+	}
+
+	return h.Sum(nil), nil
+}
+
+const dataSize = 1024 * 4
+
+func main() {
+	// setup profiler
+	options := []func(*profile.Profile){
+		profile.ProfilePath("."),
+		profile.NoShutdownHook,
+		profile.CPUProfile,
+	}
+	profiler := profile.Start(options...)
+	defer profiler.Stop()
+
+	// setup openssl
+	input := make([]byte, dataSize)
+	_, err := rand.Read(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// setup hash pool
+	hashPool := &sync.Pool{
+		New: func() interface{} { return newOpenSSLSHA256Hash() },
+	}
+
+	inputChan := make(chan []byte)
+	outputChan := make(chan []byte)
+	tasks := 1024 * 1024
+	workers := runtime.NumCPU()
+
+	for i := 0; i < workers; i++ {
+		go func() {
+			for {
+				data, ok := <-inputChan
+				if !ok {
+					return
+				}
+				output, err := compute(hashPool, data)
+				if err != nil {
+					log.Fatal(err)
+				}
+				outputChan <- output
+			}
+		}()
+	}
+
+	go func() {
+		for i := 0; i < tasks; i++ {
+			inputChan <- input
+		}
+		close(inputChan)
+	}()
+
+	var output []byte
+	for i := 0; i < tasks; i++ {
+		output = <-outputChan
+	}
+	fmt.Println(output)
+}
